f8c6a49710444a9f44d469bafff9c7c7151c012f
Wonder947 <123397090+Wonder947@users.noreply.github.com>
2023-11-01 22:25:29 -0400

    done milestone 1

--- db.mjs
+++ db.mjs
@@ -4,13 +4,42 @@ import mongoose from "mongoose";
 mongoose.connect(process.env.DSN);
 
 // my schemas go here
-const YoutubeSong = new mongoose.Schema({
+const YoutubeSongSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    accompaniment: Boolean,
+    path: {type: String, required: true}, // source of the video, currently using youtubeVideoId
+    accompaniment: {type: Boolean, required: true}, //original, or with accompaniment
     duration: String,
     description: String
 });
 
-mongoose.model('Song', YoutubeSong);
+const UserSchema = new mongoose.Schema({
+  registered: {type: Boolean, required: true},
+  username: String,
+  password: {type: String, minLength: 8},
+  history: String,
+  room: String  
+});
 
+const RoomSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    playlist: [mongoose.Types.ObjectId],
+    members: [mongoose.Types.ObjectId],
+    board: String
+})
+
+const RecordSchema = new mongoose.Schema({
+    timeAdded: {type: String, required: true},
+    songName: {type: String, required: true}
+})
+
+const HistorySchema = new mongoose.Schema({
+    user: {type: String, required: true},
+    records: [RecordSchema]
+})
+
+
+mongoose.model('Song', YoutubeSongSchema);
+mongoose.model('User', UserSchema);
+mongoose.model('Room', RoomSchema);
+mongoose.model("History", HistorySchema);
 
