c7ddec3c38da9b01403684324abd4e0061dff193
Wonder947 <123397090+Wonder947@users.noreply.github.com>
2023-11-09 18:36:35 -0500

    done add and delete songs using db schema and form

--- my-app/app/search/page.tsx
+++ my-app/app/search/page.tsx
@@ -1,5 +1,38 @@
 
-export default function Page(){
-    return <h1>This is search page!</h1>
+import '../../lib/models'
+import { songModel } from '../../lib/models'
+import { addSong } from '../actions'
+import dbConnect from '@/lib/dbConnect'
+import { deleteSong } from '../actions'
+
+function DeleteForm({songId}:{songId:string}){
+    const id = songId.toString()
+
+    return (
+        <form action={deleteSong}>
+            <input type='hidden' name='songid' value={id} />
+            <button type='submit'>Delete</button>
+        </form>
+    )
+}
+
+
+
+export default async function Page(){
+    await dbConnect()
+    const songs = await songModel.find()
+
+    return (
+        <>
+            <form action={addSong}>
+                <label htmlFor="song">Add a song name:</label>
+                <input type="text" id="song" name="song" required />
+                <button type="submit">Add</button>
+            </form>
+            <ul>
+                {songs.map((song)=>(<li key={song._id}>{song.name} <DeleteForm songId={song._id} /></li>))}
+            </ul>
+        </>
+    )
 }
 
